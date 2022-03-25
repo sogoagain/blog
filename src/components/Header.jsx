3dc2ffc565b0a4eaaf66c4ce4c5f0fd6b5fa46ca
sogoagain <imyong0@gmail.com>
2022-03-25 23:25:50 +0900

    <Header/>는 프로필 이미지를 출력한다

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Header({ title }) {
+function Header({ title, profileImage }) {
   return (
     <header>
       <h1>{title}</h1>
+      <img src={profileImage.src} alt={profileImage.alt} />
     </header>
   );
 }
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  profileImage: PropTypes.shape({
+    alt: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Header;
--- src/components/Header.test.jsx
+++ src/components/Header.test.jsx
@@ -6,14 +6,24 @@ import Header from "./Header";
 
 describe("<Header/>", () => {
   const title = "Sogoagain 블로그";
+  const profileImage = {
+    alt: "Sogoagain의 Github 프로필 이미지",
+    src: "https://avatars.githubusercontent.com/u/23417465?v=4",
+  };
 
   beforeEach(() => {
-    render(<Header title={title} />);
+    render(<Header title={title} profileImage={profileImage} />);
   });
 
   it("블로그 제목을 출력한다", () => {
     const titleEl = screen.getByText(title);
 
     expect(titleEl).toBeInTheDocument();
+  });
+
+  it("프로필 이미지를 출력한다", () => {
+    const imageEl = screen.getByAltText(profileImage.alt);
+
+    expect(imageEl).toHaveAttribute("src", profileImage.src);
   });
 });
