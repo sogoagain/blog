0b02b0556013ee226b9cd843a06bfcd011c6617b
sogoagain <imyong0@gmail.com>
2022-04-04 22:35:52 +0900

    <Footer />는 RSS 링크를 출력한다

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "gatsby";
 import React from "react";
 
-export default function Footer({ title }) {
+export default function Footer({ title, rss }) {
   const year = new Date().getFullYear();
 
   return (
     <footer>
       {title} &copy;{year}
+      <Link to={rss.to}>{rss.text}</Link>
     </footer>
   );
 }
--- src/components/Footer.test.jsx
+++ src/components/Footer.test.jsx
@@ -6,9 +6,13 @@ import Footer from "./Footer";
 
 describe("<Footer/>", () => {
   const title = "SOGOAGAIN";
+  const rss = {
+    text: "RSS",
+    to: "/rss.xml",
+  };
 
   beforeEach(() => {
-    render(<Footer title={title} />);
+    render(<Footer title={title} rss={rss} />);
   });
 
   it("블로그 제작 정보를 출력한다", () => {
@@ -16,5 +20,12 @@ describe("<Footer/>", () => {
     const copyrightEl = screen.getByText(`${title} ©${year}`);
 
     expect(copyrightEl).toBeInTheDocument();
+  });
+
+  it("RSS 링크를 출력한다", () => {
+    const rssEl = screen.getByText("RSS");
+
+    expect(rssEl).toBeInTheDocument();
+    expect(rssEl.closest("a")).toHaveAttribute("href", "/rss.xml");
   });
 });
