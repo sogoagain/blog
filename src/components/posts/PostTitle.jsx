7a5dfd511bc4b01d76881da98f6f8882ed53a2e7
sogoagain <imyong0@gmail.com>
2022-10-30 18:47:25 +0900

    PostTitle의 파일 내 컴포넌트 이름을 수정한다

--- src/components/posts/PostTitle.jsx
+++ src/components/posts/PostTitle.jsx
@@ -2,9 +2,9 @@ import styled from "@emotion/styled";
 
 import { unit } from "../../styles";
 
-const Title = styled.h1({
+const PostTitle = styled.h1({
   marginBottom: unit(1),
   lineHeight: 1.5,
 });
 
-export default Title;
+export default PostTitle;
