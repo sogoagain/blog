228094637d5a4bd2ff654b4106d6b2d3a00cc1f8
sogoagain <imyong0@gmail.com>
2023-02-02 22:36:33 +0900

    NIP-05 엔드포인트를 제공한다

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,33 @@
+const fs = require("fs-extra");
+const path = require("path");
+
 const { createFilePath } = require("gatsby-source-filesystem");
+
+const properties = require("./properties");
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
 
   if (node.internal.type === "MarkdownRemark") {
-    const { path, date } = node.frontmatter;
+    const { path: pathname, date } = node.frontmatter;
     const year = new Date(date).getFullYear();
 
-    const slug = path ? `/${year}/${path}/` : createFilePath({ node, getNode });
+    const slug = pathname
+      ? `/${year}/${pathname}/`
+      : createFilePath({ node, getNode });
 
     createNodeField({ name: "slug", value: slug, node });
   }
+};
+
+exports.onPostBuild = async () => {
+  const staticPath = `./public`;
+  const nip05Path = `.well-known/nostr.json`;
+  const outputPath = path.join(staticPath, nip05Path);
+  const outputDir = path.dirname(outputPath);
+  if (!(await fs.pathExists(outputDir))) {
+    await fs.mkdirp(outputDir);
+  }
+  const { name, pubkey } = properties.social.nostr;
+  await fs.writeFile(outputPath, JSON.stringify({ names: { [name]: pubkey } }));
 };
