bc0c16b535181318e3e4739c7325889c23644b8c
sogoagain <imyong0@gmail.com>
2023-03-05 01:53:58 +0900

    [sogoagain/blog#80] 인보이스를 복사할 수 있다 (#84)
    * 인보이스를 복사할 수 있다
    
    * InvoiceQr 컴포넌트를 추출한다
    
    * InvoiceQr을 div로 감싼다

--- src/components/support/Invoice.jsx
+++ src/components/support/Invoice.jsx
@@ -1,43 +1,49 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
 
 import styled from "@emotion/styled";
 
-import LightningIcon from "../../images/icons/lightning.png";
+import Alert from "../Alert";
+import Anchor from "../Anchor";
+import Button from "../Button";
+import InvoiceQr from "./InvoiceQr";
 
-import { unit, color } from "../../styles";
+import { unit } from "../../styles";
 
-const TextWrapper = styled.p({
-  margin: `${unit(2)} 0`,
+const BlurWrapper = styled.div({
+  filter: `blur(${unit(1)})`,
 });
 
-export default function Invoice({ invoice }) {
-  const qrRef = useRef(null);
+const CopyWrapper = styled.div({
+  marginTop: unit(3),
+});
 
-  const drawInvoiceQr = async () => {
-    const { default: QRCodeStyling } = await import("qr-code-styling");
-    const qrCodeStyling = new QRCodeStyling({
-      width: 320,
-      height: 320,
-      type: "svg",
-      data: invoice.payment_request,
-      image: LightningIcon,
-      dotsOptions: { type: "extra-rounded", color: color.primary },
-      backgroundOptions: { color: color.background },
-      cornersSquareOptions: { type: "extra-rounded", color: color.lightning },
-      cornersDotOptions: { color: color.lightning },
-    });
-    qrCodeStyling.append(qrRef.current);
+export default function Invoice({ invoice, expired }) {
+  const [copyStatus, setCopyStatus] = useState(null);
+
+  const handleCopyClick = async () => {
+    await navigator.clipboard.writeText(invoice.payment_request);
+    setCopyStatus("복사되었습니다!");
   };
 
-  useEffect(async () => {
-    await drawInvoiceQr();
-  }, []);
+  if (expired) {
+    return (
+      <BlurWrapper>
+        <InvoiceQr invoice={invoice} />
+      </BlurWrapper>
+    );
+  }
 
   return (
-    <>
-      <div data-testid="lightning-qr-element" ref={qrRef} />
-      <TextWrapper>{invoice.value.toLocaleString("en-US")} sats</TextWrapper>
-      <TextWrapper>{invoice.memo}</TextWrapper>
-    </>
+    <div>
+      <Anchor href={`lightning:${invoice.payment_request}`} target="_blank">
+        <InvoiceQr invoice={invoice} />
+      </Anchor>
+      <CopyWrapper>
+        {copyStatus && <Alert message={copyStatus} />}
+        <Button type="button" onClick={handleCopyClick}>
+          인보이스 복사하기
+        </Button>
+      </CopyWrapper>
+    </div>
   );
 }
