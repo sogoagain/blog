ce9b1230b159d05a94e8da7a72f32647e531284a
sogoagain <sogoagain@sogoagain.com>
2024-01-08 08:34:54 +0900

    빌드 오류를 수정한다

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,3 +42,11 @@ exports.onPostBuild = async () => {
     JSON.stringify({ names: { [name]: hexPubKey } })
   );
 };
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  const config = getConfig();
+  if (config.externals && config.externals[0]) {
+    config.externals[0]["node:crypto"] = require.resolve("crypto-browserify");
+  }
+  actions.replaceWebpackConfig(config);
+};
