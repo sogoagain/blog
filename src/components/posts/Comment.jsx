1e16a27ddf1a2aa722381acbd78a7487b290e1c2
sogoagain <sogoagain@sogoagain.com>
2024-01-24 09:42:05 +0900

    커멘트 영역에 마진을 추가한다

--- src/components/posts/Comment.jsx
+++ src/components/posts/Comment.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 
+import styled from "@emotion/styled";
+
 import Giscus from "@giscus/react";
+
+const Wrapper = styled.div`
+  margin: 2rem 0 1rem 0;
+`;
 
 export default function Comment({ giscus }) {
   return (
-    <Giscus
-      id="comments"
-      repo={giscus.repo}
-      repoId={giscus.repoId}
-      category={giscus.category}
-      categoryId={giscus.categoryId}
-      mapping={giscus.mapping}
-      strict="0"
-      reactionsEnabled="0"
-      emitMetadata="0"
-      inputPosition="top"
-      theme={giscus.theme}
-      lang="ko"
-    />
+    <Wrapper>
+      <Giscus
+        id="comments"
+        repo={giscus.repo}
+        repoId={giscus.repoId}
+        category={giscus.category}
+        categoryId={giscus.categoryId}
+        mapping={giscus.mapping}
+        strict="0"
+        reactionsEnabled="0"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={giscus.theme}
+        lang="ko"
+      />
+    </Wrapper>
   );
 }
