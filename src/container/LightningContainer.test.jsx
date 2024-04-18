662b20bd0a421d7738a31cf507d13acc74e6f088
sogoagain <sogoagain@sogoagain.com>
2024-04-18 10:11:05 +0900

    LightningContainer 테스트 시 0이 앞에 있는 경우도 테스트한다

--- src/container/LightningContainer.test.jsx
+++ src/container/LightningContainer.test.jsx
@@ -26,7 +26,7 @@ jest.mock("../services/blog");
 describe("<LightningContainer/>", () => {
   function requestInvoice() {
     fireEvent.change(screen.getByPlaceholderText("2100"), {
-      target: { value: "9a4b0c9d" },
+      target: { value: "09a4b0c9d" },
     });
     fireEvent.change(screen.getByPlaceholderText("PIZZA 🍕"), {
       target: { value: "오 ~ 막걸리 좋아요!" },
