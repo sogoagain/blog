cd2ad43a84a7f0e95a2ba3cf6af9f909b5a6d19a
SOGOAGAIN⚡️ <sogoagain@sogoagain.com>
2024-04-18 22:33:21 +0900

    [sogoagain/blog#178] `Alert` - `word-break` 속성 추가  (#181)

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const AlertWrapper = styled.div`
+  word-break: keep-all;
+  overflow-wrap: break-word;
   border: 1px solid;
   padding: 0.1rem 0.3rem;
   text-align: center;
