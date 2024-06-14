3f57475e338b8b4a72eb49e4a53696f4cbb26d14
SOGOAGAIN⚡️ <sogoagain@sogoagain.com>
2024-06-14 23:12:46 +0900

    [sogoagain/blog#183] 로고 디자인에 맞게 타이틀 수정 (#184)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
+import styled from "@emotion/styled";
+
 import InterestsContainer from "../container/InterestsContainer";
 import LayoutContainer from "../container/LayoutContainer";
 import PostsContainer from "../container/PostsContainer";
 import SeoContainer from "../container/SeoContainer";
 import PostsTagListContainer from "../container/PostsTagListContainer";
 
+const Title = styled.h1`
+  line-height: 1;
+  font-family: monospace;
+`;
+
 export default function IndexPage({ location }) {
   return (
     <LayoutContainer location={location}>
       <SeoContainer titleTemplate="%s 블로그" pathname={location.pathname} />
-      <h1>SOGOAGAIN</h1>
+      <Title>
+        SO,
+        <br />
+        GO
+        <br />
+        AGAIN
+      </Title>
       <InterestsContainer />
       <PostsTagListContainer />
       <hr />
