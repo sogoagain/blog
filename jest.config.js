4f11ccb7261f4953a817cae1c3b6b8ac459508f2
sogoagain <hello@sogoagain.com>
2025-12-27 18:26:03 +0900

    gatsby, jest 설정 파일을 간소화한다

--- jest.config.js
+++ jest.config.js
@@ -15,9 +15,6 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testEnvironmentOptions: {
-    url: `http://localhost`,
-  },
   setupFiles: [`<rootDir>/loadershim.js`, "jest-canvas-mock"],
   setupFilesAfterEnv: [
     "<rootDir>/jest.setup.js",
